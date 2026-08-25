18d1fb9e25f4ff5b4ca5a8d4e7514d7ebf7a9e17
Anders Engström <engand10@gmail.com>
2026-08-25 20:13:23 +0200

    Send Supabase magic-link redirect correctly

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "medicinlogg-v17";
+const cacheName = "medicinlogg-v18";
 const filesToCache = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./config.js", "./cloud-sync.js", "./privacy/", "./oauth/consent/", "./oauth/consent/consent.js"];
 
 self.addEventListener("install", (event) => {
